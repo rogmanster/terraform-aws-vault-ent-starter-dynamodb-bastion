5e41fa43691bf8e1a9c6152172c5d62f8caf1629
rogmanster <rogman@gmail.com>
2022-05-02 18:59:04 -0700

    output key_name for vault nodes

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,7 @@ output "telemetry_public_ip" {
 output "telemetry_public_dns" {
   value = aws_instance.telemetry.*.public_dns
 }
+
+output "key_name" {
+  value = tls_private_key.awskey.public_key_openssh
+}
